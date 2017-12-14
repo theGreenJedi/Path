ae9ae9557877c0f6f676057daf1293d77e6d13f0
theGreenJedi <theGreenJedi@protonmail.com>
2017-12-14 15:44:57 -0500

    magneta

---
+++ Muzak/magenta/kokoro/test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Fail on any error.
+set -e
+# Display commands being run.
+set -x
+
+eval "${PIP_COMMAND} install --upgrade tensorflow scipy matplotlib \
+  intervaltree bokeh IPython librosa mir_eval"
+
+bazel test \
+  --keep_going \
+  --test_output=errors \
+  ${BAZEL_TEST_ARGS} \
+  -- \
+  //magenta/...
